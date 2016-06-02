b258f63a803b840d8bf09e09a04e16d619a01ade
Chuck May <mayc@imsweb.com>
2016-06-02 14:40:20 -0400

    Stop using absolute paths!

--- src/test/java/com/imsweb/staging/UpdaterUtils.java
+++ src/test/java/com/imsweb/staging/UpdaterUtils.java
@@ -40,7 +40,7 @@ import com.imsweb.staging.util.Stopwatch;
  */
 public final class UpdaterUtils {
 
-    private static final String _BASE_DIRECTORY = "C:/Prj/staging-client-java/src/main/resources/algorithms";
+    private static final String _BASE_DIRECTORY = "src/main/resources/algorithms";
 
     private static Pattern _ID_CHARACTERS = Pattern.compile("[a-z0-9_]+");
 
