588908061de7b54bb78a2b9254d621175d7fd7b3
Chuck May <mayc@imsweb.com>
2017-09-27 11:54:15 -0400

    Working on unit test

--- src/main/java/com/imsweb/staging/ExternalStagingFileDataProvider.java
+++ src/main/java/com/imsweb/staging/ExternalStagingFileDataProvider.java
@@ -12,10 +12,8 @@ import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.imsweb.staging.entities.StagingSchema;
 import com.imsweb.staging.entities.StagingTable;
@@ -43,47 +41,50 @@ public class ExternalStagingFileDataProvider extends StagingDataProvider {
         Set<String> algorithms = new HashSet<>();
         Set<String> versions = new HashSet<>();
 
-        // loop over all tables and load them into Map
-        try {
-            for (String file : readLines(fs, "tables/ids.txt")) {
-                if (!file.isEmpty()) {
-                    try (BufferedReader reader = Files.newBufferedReader(fs.getPath("tables/" + file + ".json"))) {
-                        StagingTable table = getMapper().reader().readValue(getMapper().getFactory().createParser(reader), StagingTable.class);
+        // iterate over files in the zip file and process schemas and tables
+        fs.getRootDirectories()
+                .forEach(root -> {
+                    try {
+                        Files.walk(root)
+                                .filter(path -> Files.isRegularFile(path) && path.toString().contains(".json"))
+                                .forEach(path -> {
+                                            if (path.startsWith("/tables/")) {
+                                                try (BufferedReader reader = Files.newBufferedReader(path)) {
+                                                    StagingTable table = getMapper().reader().readValue(getMapper().getFactory().createParser(reader), StagingTable.class);
 
-                        initTable(table);
+                                                    initTable(table);
 
-                        algorithms.add(table.getAlgorithm());
-                        versions.add(table.getVersion());
+                                                    algorithms.add(table.getAlgorithm());
+                                                    versions.add(table.getVersion());
 
-                        _tables.put(table.getId(), table);
+                                                    _tables.put(table.getId(), table);
+                                                }
+                                                catch (IOException e) {
+                                                    throw new IllegalStateException("Exception processing table: " + e.getMessage());
+                                                }
+                                            }
+                                            else if (path.startsWith("/schemas")) {
+                                                try (BufferedReader reader = Files.newBufferedReader(path)) {
+                                                    StagingSchema schema = getMapper().reader().readValue(getMapper().getFactory().createParser(reader), StagingSchema.class);
+
+                                                    initSchema(schema);
+
+                                                    algorithms.add(schema.getAlgorithm());
+                                                    versions.add(schema.getVersion());
+
+                                                    _schemas.put(schema.getId(), schema);
+                                                }
+                                                catch (IOException e) {
+                                                    throw new IllegalStateException("Exception processing schema: " + e.getMessage());
+                                                }
+                                            }
+                                        }
+                                );
                     }
-                }
-            }
-        }
-        catch (IOException e) {
-            throw new IllegalStateException("Exception reading tables: " + e.getMessage());
-        }
-
-        // loop over all schemas and load them into Map
-        try {
-            for (String file : readLines(fs, "schemas/ids.txt")) {
-                if (!file.isEmpty()) {
-                    try (BufferedReader reader = Files.newBufferedReader(fs.getPath("schemas/" + file + ".json"))) {
-                        StagingSchema schema = getMapper().reader().readValue(getMapper().getFactory().createParser(reader), StagingSchema.class);
-
-                        initSchema(schema);
-
-                        algorithms.add(schema.getAlgorithm());
-                        versions.add(schema.getVersion());
-
-                        _schemas.put(schema.getId(), schema);
+                    catch (IOException e) {
+                        throw new IllegalStateException("Exception processing external algorithm: " + e.getMessage());
                     }
-                }
-            }
-        }
-        catch (IOException e) {
-            throw new IllegalStateException("Exception reading schemas: " + e.getMessage());
-        }
+                });
 
         if (algorithms.size() != 1)
             throw new IllegalStateException("Error initializing provider; only one algorithm should be included in file");
@@ -95,17 +96,6 @@ public class ExternalStagingFileDataProvider extends StagingDataProvider {
 
         // finally, initialize any caches now that everything else has been set up
         invalidateCache();
-    }
-
-    /**
-     * @param location relative file location within the classpath
-     * @return a {@link String} {@link List} of all lines in the file
-     * @throws IOException error reading file
-     */
-    private static List<String> readLines(FileSystem fs, String location) throws IOException {
-        try (BufferedReader buffer = Files.newBufferedReader(fs.getPath(location))) {
-            return buffer.lines().collect(Collectors.toList());
-        }
     }
 
     @Override
--- src/test/java/com/imsweb/staging/ExternalStagingFileDataProviderTest.java
+++ src/test/java/com/imsweb/staging/ExternalStagingFileDataProviderTest.java
@@ -1,6 +1,8 @@
 package com.imsweb.staging;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,15 +15,19 @@ public class ExternalStagingFileDataProviderTest {
 
     @BeforeClass
     public static void setup() throws IOException {
-        _STAGING = Staging.getInstance(new ExternalStagingFileDataProvider("c:/tmp/eod1.0.zip"));
+        // need way to get absolute path of the testing external algorithm; it also needs to use forward slashes
+        Path resourceDirectory = Paths.get("src/test/resources/external_algorithm.zip");
+        String absolutePath = resourceDirectory.toAbsolutePath().toString().replaceAll("\\\\", "/");
+
+        _STAGING = Staging.getInstance(new ExternalStagingFileDataProvider(absolutePath));
     }
 
     @Test
     public void testExternalLoad() throws IOException {
-        assertEquals("eod", _STAGING.getAlgorithm());
-        assertEquals("1.0", _STAGING.getVersion());
-        assertEquals(116, _STAGING.getSchemaIds().size());
-        assertEquals(1630, _STAGING.getTableIds().size());
+        assertEquals("testing", _STAGING.getAlgorithm());
+        assertEquals("99.99", _STAGING.getVersion());
+        assertEquals(1, _STAGING.getSchemaIds().size());
+        assertEquals(62, _STAGING.getTableIds().size());
     }
 
 }
