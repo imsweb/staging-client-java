54e3184f97bac6d5b4a5736ae5d71c78b3a2acd3
Chuck May <mayc@imsweb.com>
2019-01-18 16:32:42 -0500

    Trying out a cache replacement that is compliant with Java 9+.
    Caffeine has worked well but still uses the UnSafe class which is
    deprecated.

--- src/main/java/com/imsweb/staging/StagingDataProvider.java
+++ src/main/java/com/imsweb/staging/StagingDataProvider.java
@@ -15,13 +15,14 @@ import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.cache2k.Cache;
+import org.cache2k.Cache2kBuilder;
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
 
 import com.imsweb.decisionengine.ColumnDefinition.ColumnType;
 import com.imsweb.decisionengine.DataProvider;
@@ -70,19 +71,26 @@ public abstract class StagingDataProvider implements DataProvider {
     }
 
     // lookup cache
-    private LoadingCache<SchemaLookup, List<StagingSchema>> _lookupCache;
+    private Cache<SchemaLookup, List<StagingSchema>> _lookupCache;
     // site/hist cache
-    private LoadingCache<String, Set<String>> _validValuesCache;
+    private Cache<String, Set<String>> _validValuesCache;
 
     /**
      * Constructor loads all schemas and sets up cache
      */
     protected StagingDataProvider() {
         // cache schema lookups
-        _lookupCache = Caffeine.newBuilder().maximumSize(500).expireAfterWrite(10, TimeUnit.MINUTES).build(this::getSchemas);
+        _lookupCache = new Cache2kBuilder<SchemaLookup, List<StagingSchema>>() {}
+                .entryCapacity(500)
+                .expireAfterWrite(10, TimeUnit.MINUTES)    // expire/refresh after 5 minutes
+                .resilienceDuration(30, TimeUnit.SECONDS) // cope with at most 30 seconds
+                .loader(this::getSchemas)         // auto populating function
+                .build();
 
         // cache the valid values for certain tables including site and histology
-        _validValuesCache = Caffeine.newBuilder().build(this::getAllInputValues);
+        _validValuesCache = new Cache2kBuilder<String, Set<String>>() {}
+                .loader(this::getAllInputValues)         // auto populating function
+                .build();
     }
 
     /**
@@ -300,8 +308,9 @@ public abstract class StagingDataProvider implements DataProvider {
      * Clear the caches
      */
     public void invalidateCache() {
-        _lookupCache.invalidateAll();
-        _validValuesCache.invalidateAll();
+        // TODO not sure how to do this?
+        //_lookupCache.invalidateAll();
+        //_validValuesCache.invalidateAll();
     }
 
     /**
