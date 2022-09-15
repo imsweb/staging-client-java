6938292216fed168cca21043aaa048f314192e99
Chuck May <mayc@imsweb.com>
2022-09-15 14:11:07 -0400

    Updated Toronto algorithm to 0.2

--- src/test/java/com/imsweb/staging/toronto/TorontoUpdateFromAPI.java
+++ src/test/java/com/imsweb/staging/toronto/TorontoUpdateFromAPI.java
@@ -7,14 +7,14 @@ import java.util.HashSet;
 import com.imsweb.staging.update.UpdaterUtils;
 
 /**
- * Update the EOD data from the API
+ * Update the Toronto data from the API
  */
 public class TorontoUpdateFromAPI {
 
     private static final String _ALGORITHM = "toronto";
 
     public static void main(String[] args) throws IOException {
-        UpdaterUtils.update(_ALGORITHM, "0.1", new HashSet<>(Collections.singletonList("STAGING")));
+        UpdaterUtils.update(_ALGORITHM, "0.2", new HashSet<>(Collections.singletonList("STAGING")));
     }
 
 }
