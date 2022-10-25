4ffa1c853941f2a743a90bc8d90dfa7bc36961b4
Chuck May <mayc@imsweb.com>
2022-10-25 13:45:15 -0400

    Fixed zipAlgorithms

--- lib/src/test/java/com/imsweb/staging/zip/ZipAlgorithms.java
+++ lib/src/test/java/com/imsweb/staging/zip/ZipAlgorithms.java
@@ -13,11 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeroturnaround.zip.ZipUtil;
 
-//import com.imsweb.staging.cs.CsDataProvider.CsVersion;
-//import com.imsweb.staging.eod.EodDataProvider.EodVersion;
-//import com.imsweb.staging.tnm.TnmDataProvider.TnmVersion;
-//import com.imsweb.staging.toronto.TorontoDataProvider.TorontoVersion;
-
 /**
  * Create ZIP files of algorithms to include in releases
  */
@@ -26,15 +21,15 @@ public class ZipAlgorithms {
     private static final Logger _LOG = LoggerFactory.getLogger(ZipAlgorithms.class);
 
     public static void main(String[] args) throws IOException {
-//        zipAlgorithm("cs", CsVersion.LATEST.getVersion());
-        //        zipAlgorithm("tnm", TnmVersion.LATEST.getVersion());
-        //        zipAlgorithm("eod_public", EodVersion.LATEST.getVersion());
-        //        zipAlgorithm("toronto", TorontoVersion.LATEST.getVersion());
+        zipAlgorithm("algorithm-cs", "cs", "02.05.50");
+        zipAlgorithm("algorithm-tnm", "tnm", "2.0");
+        zipAlgorithm("algorithm-eod", "eod_public", "3.0");
+        zipAlgorithm("algorithm-toronto", "toronto", "0.3");
     }
 
-    private static void zipAlgorithm(String algorithm, String version) throws IOException {
-        Path algoDir = Paths.get("src", "main", "resources", "algorithms", algorithm, version);
-        Path buildDir = Paths.get("build", "algorithms");
+    private static void zipAlgorithm(String subproject, String algorithm, String version) throws IOException {
+        Path algoDir = Paths.get("..", subproject, "src", "main", "resources", "algorithms", algorithm, version);
+        Path buildDir = Paths.get("..", "build", "algorithms");
         String outputFile = buildDir.toFile().getAbsolutePath() + File.separatorChar + algorithm + "-" + version + ".zip";
 
         // make sure directory exists
