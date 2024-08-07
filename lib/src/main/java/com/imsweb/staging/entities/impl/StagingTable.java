00da82a8485c97f9c854b1824233953ce520e8f9
Chuck May <mayc@imsweb.com>
2024-08-07 16:22:48 -0400

    Fixed error

--- lib/src/main/java/com/imsweb/staging/entities/impl/StagingTable.java
+++ lib/src/main/java/com/imsweb/staging/entities/impl/StagingTable.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
